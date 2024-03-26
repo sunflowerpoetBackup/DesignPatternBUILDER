3e5d52189e25310a3d00b78ef9f31588411a47a9
sunflowerpoet <anna.simionato747@gmail.com>
2024-03-26 10:05:24 +0100

    FIRST

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,19 @@
 //Creare una classe Builder che attraverso il costruttore e dei metodi specifici
 // (tranne nome e cognome, gli altri campi sono opzionali) per costruire l'oggetto Person.
 //Creare due oggetti Person e stamparli a video
-
+/*Crea una classe Person con i campi firstName, lastName, age e address, i getter and i setter
+Il costruttore accetterà un Builder in input
+Creare una classe Builder che attraverso il costruttore e dei metodi specifici (tranne nome e cognome, gli altri campi sono opzionali) per costruire l'oggetto Person.
+Creare due oggetti Person e stamparli a video*/
 public class Main {
     public static void main(String[] args) {
 
-        Persona n1 = new Persona(new Builder("Margherita", "Rossi"));
-        n1.setAge(29);
-        n1.setAddress("Via Roma 10, Milano");
+        Persona n1 = new Persona(new Builder("Anna", "Simionato"));
+        n1.setAge(23);
+        n1.setAddress("Via Rizzi 10, Padova");
 
-        Persona n2 = new Persona(new Builder("Carmen", "Verdi"));
-        n2.setAge(98);
+        Persona n2 = new Persona(new Builder("Alex", "Verdi"));
+        n2.setAge(27);
 
         System.out.println(n1);
         System.out.println(n2);
